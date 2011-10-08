95843960b8c84c889898cf37030e21b21d9087b4
chenzhi <chenzhi@6ec6bfb8-d06b-6c47-a642-727affc0819e>
2011-10-08 01:54:30 +0000

    加入开始和结束批处理文件
    Signed-off-by: chenzhi <18607916309@163.com>
    
    git-svn-id: https://172.16.100.245:443/svn/yihunze@25 6ec6bfb8-d06b-6c47-a642-727affc0819e

---
+++ server/yihunzeServer/bin/startserver_d.bat
@@ -0,0 +1,5 @@
+start AccountServer/debug/accountserver_d.exe
+start stateserver/debug/stateserver_d.exe
+start GameDatabaseServer/debug/GameDataServer_d.exe
+start gameserver/debug/gameserver_d.exe
+start SimulateClient/debug/SimulateClient_d.exe
---
+++ server/yihunzeServer/bin/stopserver_d.bat
@@ -0,0 +1,5 @@
+tskill  accountserver_d
+tskill  stateserver_d
+tskill  GameDataServer_d
+tskill  gameserver_d
+tskill  SimulateClient_d
